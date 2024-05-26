5d19a1b61242fc63edad078037869cdf3f7628a9
Cadu Higueras <cadu.higueras@gmail.com>
2024-05-26 10:19:49 +0200

    Reading command line arguments

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    // NOTE: if the program needs support invalid unicode use std::env::args_os
+    // NOTE: args() returns an iterator and collect turn the iterator into a Vector in this case
+    let args: Vec<String> = env::args().collect();
+    // dbg!(args);
+
+    let query = &args[1];
+    let file_path = &args[2];
+
+    // println!("Searching for {}", query);
+    // println!("In file {}", file_path);
 }
